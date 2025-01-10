427d1096263c0f57cd2487a9fe00c54dba1f2176
vaibhav_xi <vaibhav28890@gmail.com>
2025-01-10 22:50:49 +0530

    get a list of risk amounts if we keep losing bruh

--- investment.py
+++ investment.py
@@ -1,3 +1,20 @@
+def risk_amounts(initial_amount, max_tries):
+    i = 1
+    amounts = []
+    
+    amounts.append(initial_amount)
+    
+    while True:
+        if i > max_tries:
+            return amounts
+        
+        initial_amount = initial_amount * 2
+        
+        amounts.append(initial_amount)
+        
+        i += 1
+    
+
 def starting_amount(x, max_tries):
     
     # Calculate the sum of the geometric series: 2^10 - 1
@@ -11,4 +28,7 @@ total_coins = 100000
 max_tries = 10
 
 initial_risk = starting_amount(total_coins, max_tries)
+risk_list = risk_amounts(initial_risk, max_tries)
+
 print(f"Initial amount: {initial_risk}")
+print(f"Risk Amounts: {risk_list}")
